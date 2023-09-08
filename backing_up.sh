a5472f15755d467b2d1f5869f149f5fe3b11785d
Francisco Vasconcelos Junior <juniorphy@gmail.com>
2023-09-08 20:22:02 -0300

    update script to back up.

--- backing_up.sh
+++ backing_up.sh
@@ -1,5 +1,20 @@
+#!/bin/bash
 
-rsync -r dcppA-hindcast/* /Volumes/GoogleDrive-106379421171705090873/My\ Drive/PROJETOS/dccp/dcppA-hindcast/
+for EXP in 'A-hindcast' 'B-forecast' ; do 
 
-rsync -r dcppA-hindcast/*  /Volumes/Lenovo/dcpp-decadal/dcppA-hindcast/.
+echo $EXP gdrive
 
+rsync -rv dcpp${EXP}/* /Volumes/GoogleDrive-106379421171705090873/My\ Drive/PROJETOS/dccp/dcpp${EXP}/.
+
+echo $EXP HD backup
+
+rsync -rv dcpp${EXP}/*  /Volumes/Lenovo/dcpp-decadal/dcpp${EXP}/.
+
+done
+
+exit 1
+
+
+rsync -r dcppB-hindcast/* /Volumes/GoogleDrive-106379421171705090873/My\ Drive/PROJETOS/dccp/dcppB-hindcast/
+
+rsync -r dcppB-hindcast/*  /Volumes/Lenovo/dcpp-decadal/dccpB-hindcast/.
